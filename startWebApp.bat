1e1d8f6aa601dacb65a1d6de91103bb40fab17da
IO-763 <IO-763@LAPTOP-8Q0011J1>
2020-06-16 14:21:53 +0900

    20200616

--- startWebApp.bat
+++ startWebApp.bat
@@ -1,8 +1,8 @@
 rem compile java files
-javac -d WebContent\WEB-INF\classes -cp C:\Apache\apache-tomcat-9.0.36\lib\* src\bingo\WSServlet.java src\bingo\Roulette.java src\bingo\DBHandler.java
+javac -d WebContent\WEB-INF\classes -cp C:\pleiades\tomcat\9\lib\* src\bingo\WSServlet.java src\bingo\Roulette.java src\bingo\DBHandler.java
 
 rem start Apache Tomcat
-C:\Apache\apache-tomcat-9.0.36\bin\startup.bat
+C:\pleiades\tomcat\9\bin\startup.bat
 
 rem start Button
 rem java -cp WebContent\WEB-INF\classes bingo.RouletteButton
